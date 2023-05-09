b06d551b71165d3776d039017f726e82e2ebf426
Dilay Kirisci <isik.kirisci@gmail.com>
2023-05-09 15:01:40 +0300

    Router setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import { Route, Routes, NavLink } from "react-router-dom";
+import { Login } from "./components/Login";
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<Routes>
+				<Route path="/" />
+			</Routes>
+
+			<NavLink path="/Login" element={Login} LOGIN />
+		</div>
+	);
 }
 
 export default App;
